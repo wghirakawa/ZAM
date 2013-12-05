8b28c85f4b98f545f379a7a6adfc37b6e4174ac7
Wayne Grimes Hirakawa <himobileappdev@gmail.com>
2013-12-04 15:22:16 -1000

    Dec 4. 2013 3:22:09

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -4,8 +4,11 @@ var AppControllers = angular.module('controllers', []);
 AppControllers.controller('MyCtrl1', ['$scope', '$http', 
 	function($scope, $http){
 		$http.get('/users').success(function(data){
-			$scope.results = data.places;	
+			$scope.results = data.places;
+			$scope.count = data.places.length;	
 		});
+		
+		$scope.test2 = "Hello Derek";
 		
 	}//End of MyCtrl1 Function
 ]);
